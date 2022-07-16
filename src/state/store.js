baec76e547ab6391bb2f95784ecad81ffbef7448
Eric <eocebreiro@gmail.com>
2022-07-16 21:36:34 +0900

    added alert reducer and actions

--- src/state/store.js
+++ src/state/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 
+import alertReducer from "./alert/alertReducer";
 import authReducer from "./auth/authReducer";
 import profileReducer from "./profile/profileReducer";
 
 const preloadedState = {};
 
 const rootReducer = combineReducers({
+  alert: alertReducer,
   auth: authReducer,
   profile: profileReducer,
 });
